356d93ec6b4f63435067beab477d3ca6de863167
ehrmaks <qwefk123@naver.com>
2020-04-12 04:17:17 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="gray-background">
-//       <img src={logo} lat="logo" />
-//       <h2>Let's develop management system!</h2>
-//     </div>
-//   );
-// }
-
 class App extends Component {
   render() {
     return (
