431ea63466ff4d04c1d2526c5cf8d7dfac1ec4a8
ehrmaks <qwefk123@naver.com>
2020-04-12 22:40:45 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': '김진석',
-        'birthday': '910929',
-        'gender': '남자',
-        'job': 'Developer'
-        },
-        {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': '홍길동',
-        'birthday': '001101',
-        'gender': '여자',
-        'job': '프리랜서'
-        },
-        {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': '이수',
-        'birthday': '890101',
-        'gender': '남자',
-        'job': '가수'
+    connection.query(
+        "SELECT * FROM CUSTOMER", (err, rows, fields) => {
+            res.send(rows);
         }
-    ])
+    )
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
