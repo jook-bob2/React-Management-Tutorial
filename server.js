c813976769b2b2f9022b68b137b323a0ccbe7abe
ehrmaks <qwefk123@naver.com>
2020-04-13 20:49:10 +0900

    Add Customer Delete Module

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ const upload = multer({dest: './upload'});
 
 app.get('/api/customers', (req, res) => {
     connection.query(
-        "SELECT * FROM CUSTOMER", (err, rows, fields) => {
+        "SELECT * FROM CUSTOMER WHERE isDeleted = 0", 
+            (err, rows, fields) => {
             res.send(rows);
         }
     );
@@ -34,13 +35,23 @@ app.get('/api/customers', (req, res) => {
 app.use('/image',express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'),(req, res) => {
-    let sql = 'INSERT INTO CUSTOMER VALUES (NULL,?,?,?,?,?)';
+    let sql = 'INSERT INTO CUSTOMER VALUES (NULL, ?, ?, ?, ?, ?, NOW(), 0)';
     let image = '/image/' + req.file.filename;
     let name = req.body.name;
     let birthday = req.body.birthday;
     let gender = req.body.gender;
     let job = req.body.job;
     let params = [image, name, birthday, gender, job];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        })
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+    let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+    let params = [req.params.id];
+
     connection.query(sql, params,
         (err, rows, fields) => {
             res.send(rows);
